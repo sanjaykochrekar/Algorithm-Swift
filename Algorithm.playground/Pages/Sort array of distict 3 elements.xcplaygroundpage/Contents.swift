aa67222bcc78b9486188ccb70b52e7ca0e7abfc4
sanjay <kochrekarsanjay87@gmail.com>
2023-08-30 00:33:55 +0530

    dutch national flag problem

--- Algorithm.playground/Pages/Sort array of distict 3 elements.xcplaygroundpage/Contents.swift
+++ Algorithm.playground/Pages/Sort array of distict 3 elements.xcplaygroundpage/Contents.swift
@@ -1,16 +1,20 @@
 // Sort array of distict 3 elements (0, 1, 2)
 
 extension Array where Element : Comparable {
-    mutating func sort(smaller: Element, greater: Element) {
+    mutating func sort012(smaller: Element, greater: Element) {
         var firstIndex: Int = 0
         var lastindex: Int = self.count - 1
+        
         for (index, _) in self.enumerated() {
-            if self[firstIndex] == smaller {
+            if self[index] == smaller {
                 (self[firstIndex], self[index]) = (self[index], self[firstIndex])
                 firstIndex = firstIndex + 1
             }
-            if self[lastindex] == greater {
-                (self[lastindex], self[index]) = (self[index], self[lastindex])
+        }
+        
+        for (index, _) in self.enumerated() {
+            if self[self.count - 1 - index] == greater {
+                (self[lastindex], self[self.count - 1 - index]) = (self[self.count - 1 - index], self[lastindex])
                 lastindex = lastindex - 1
             }
         }
@@ -19,11 +23,17 @@ extension Array where Element : Comparable {
 
 
 
-var array = [0,2,1,0, 1,0, 1,2,1,0]
+var array = [0, 2, 1, 0, 1, 0, 1, 2, 1, 0, 2]
 
-array.sort()
+array.sort012(smaller: 0, greater: 2)
+print(array)
+
+
 var array2 = ["a", "c", "a", "b"]
 var array1 = [0, 2, 0, 1,2]
 
-array2.sort()
-array1.sort()
+array2.sort012(smaller: "a", greater: "c")
+print(array2)
+
+array1.sort012(smaller: 0, greater: 2)
+print(array1)
